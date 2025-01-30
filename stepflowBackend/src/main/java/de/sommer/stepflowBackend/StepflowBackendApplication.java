ce937795d281aa57db58cc858bc8ab9def9f52e0
Tim Sommer <151786923+DrSommer20@users.noreply.github.com>
2025-01-30 21:42:47 +0100

    Prepare for history rewrite: remove .env and application.properties

--- stepflowBackend/src/main/java/de/sommer/stepflowBackend/StepflowBackendApplication.java
+++ stepflowBackend/src/main/java/de/sommer/stepflowBackend/StepflowBackendApplication.java
@@ -3,10 +3,13 @@ package de.sommer.stepflowBackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class StepflowBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("src/main/resources").filename(".env").ignoreIfMissing().ignoreIfMalformed().load();
 		SpringApplication.run(StepflowBackendApplication.class, args);
 	}
 
